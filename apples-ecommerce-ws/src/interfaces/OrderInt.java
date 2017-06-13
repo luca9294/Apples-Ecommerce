539f8a8d6e959b0079389510d724c80b40cb4fe9
luca9294 <luca9294@hotmail.it>
2017-06-13 08:25:59 +0200

    Add files via upload
    OrderInt

---
+++ apples-ecommerce-ws/src/interfaces/OrderInt.java
@@ -0,0 +1,46 @@
+package interfaces;
+
+import java.util.ArrayList;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
+public interface OrderInt {
+	
+	@WebMethod
+	/**
+	 * Add a news Order
+	 * @param cart_id, customer_id
+	 * @return
+	 */
+	public boolean addOrder (int cart_id, int customer_id);
+
+	@WebMethod
+	/**
+	 * Change the current status of the order
+	 * @param cart_id, newStatus
+	 * @return
+	 */
+	public boolean changeOrderStatus (int order_id, int newStatus);
+	
+	 @WebMethod
+	/**
+	 * Delete Order
+	 * @param order_id
+	 * @return
+	 */
+	public boolean deleteOrder(int order_id);
+	
+		 @WebMethod
+	/**
+	 * Return Orders current customer
+	 * @param cart_id
+	 * @return
+	 */
+	public OrderObject[] getOrders(int customer_id);
+	
+	
+	
+	
+	
+}
